9954f53e2cbcfd8650dd7c9d6ee7891c6e0671f1
Nguyễn Viết Hoàng <hoangnv10052004@gmail.com>
2026-08-20 22:58:20 +0700

    Add new error codes for session continuation handling
    Added ALREADY_CONTINUATION_SESSION and INTERRUPTION_THRESHOLD_NOT_MET to ClassSessionErrorCodes to support better management of interrupted or continuation class sessions.

--- MV.DomainLayer/Exceptions/ClassSessionErrorCodes.cs
+++ MV.DomainLayer/Exceptions/ClassSessionErrorCodes.cs
@@ -24,4 +24,8 @@ public static class ClassSessionErrorCodes
     public const string ConfirmDeadlinePassed = "CONFIRM_DEADLINE_PASSED";
     public const string UnauthorizedAccess = "UNAUTHORIZED_ACCESS";
     public const string DisputeAlreadyExists = "DISPUTE_ALREADY_EXISTS";
+
+    // Buổi học bị ngắt giữa chừng / buổi phụ
+    public const string AlreadyContinuationSession = "ALREADY_CONTINUATION_SESSION";
+    public const string InterruptionThresholdNotMet = "INTERRUPTION_THRESHOLD_NOT_MET";
 }
